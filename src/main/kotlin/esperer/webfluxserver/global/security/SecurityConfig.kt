12c12750fd94e590dc4c3b67410a012408ada630
aesperer <piyrw9754@gmail.com>
2023-09-05 08:16:20 +0900

    create :: security config

---
+++ src/main/kotlin/esperer/webfluxserver/global/security/SecurityConfig.kt
@@ -0,0 +1,25 @@
+package esperer.webfluxserver.global.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+
+    @Bean
+    protected fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
+        http
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .csrf { it.disable() }
+            .authorizeExchange { it.anyExchange().permitAll() }
+            .build()
+
+    @Bean
+    protected fun passwordEncoder() = BCryptPasswordEncoder()
+}
