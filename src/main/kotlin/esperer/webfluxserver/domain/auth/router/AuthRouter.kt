8220dfe0a7db2357abdd9a9b19938e8c39863251
aesperer <piyrw9754@gmail.com>
2023-09-05 10:24:14 +0900

    create :: auth router

---
+++ src/main/kotlin/esperer/webfluxserver/domain/auth/router/AuthRouter.kt
@@ -0,0 +1,19 @@
+package esperer.webfluxserver.domain.auth.router
+
+import esperer.webfluxserver.domain.auth.handler.AuthHandler
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.coRouter
+
+@Configuration
+class AuthRouter {
+
+    @Bean
+    fun authBaseRouter(authHandler: AuthHandler) = coRouter {
+        "users".nest {
+            contentType(MediaType.APPLICATION_JSON)
+            POST("/signup", authHandler::signUp)
+        }
+    }
+}
