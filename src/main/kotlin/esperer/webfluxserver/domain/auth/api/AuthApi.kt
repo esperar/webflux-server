bf71f22a9209e00cfe46c8e3dc5e2faea3945773
aesperer <piyrw9754@gmail.com>
2023-09-05 10:26:28 +0900

    create :: auth api

--- src/main/kotlin/esperer/webfluxserver/domain/auth/api/AuthApi.kt
+++ src/main/kotlin/esperer/webfluxserver/domain/auth/api/AuthApi.kt
@@ -1,11 +1,14 @@
 package esperer.webfluxserver.domain.auth.api
 
+import esperer.webfluxserver.domain.auth.dto.SignInRequest
 import esperer.webfluxserver.domain.auth.dto.SignUpRequest
 import esperer.webfluxserver.domain.auth.dto.TokenResponse
 import esperer.webfluxserver.domain.user.constant.Authority
 import esperer.webfluxserver.domain.user.entity.UserEntity
 import esperer.webfluxserver.domain.user.exception.UserAlreadyExistsException
+import esperer.webfluxserver.domain.user.exception.UserNotFoundException
 import esperer.webfluxserver.domain.user.repository.UserRepository
+import esperer.webfluxserver.global.error.exception.UnAuthorizedException
 import esperer.webfluxserver.global.security.token.JwtProvider
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -31,6 +34,18 @@ class AuthApi(
                 authority = Authority.ROLE_USER
             )
         )
+
+        return jwtProvider.getToken(user.id)
+    }
+
+    suspend fun signIn(request: SignInRequest): TokenResponse {
+        val user = userRepository.findByEmail(request.email)
+            ?: throw UserNotFoundException()
+
+        val isMatches = passwordEncoder.matches(user.password, request.password)
+
+        if(isMatches)
+            throw UnAuthorizedException()
 
         return jwtProvider.getToken(user.id)
     }
